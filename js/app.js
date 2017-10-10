90ee9c74698067ad624dc22336e57d414ddb5d9c
margb13 <marg.b13@hotmail.com>
2017-10-10 18:12:29 -0500

    v1

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+var msj = document.getElementById("msj"); // variable resultado es el lugar donde mi elemento que se encuentra en mi html donde me mostrara el resultado el valor unicode 
+
+var mensaje = prompt('Ingrese su numero de tarjeta:' );
+var respuesta = mensaje;
+if (mensaje.length > 0){
+  alert('ingresa un numero por favor');
+}
+var arr = [];
+var isValidCard = function(arr){
+
+var str = respuesta.split();
+
+
+
+ 
+};
